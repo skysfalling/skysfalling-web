515cbb079cd5357caace88586423590d742b5725
Sky Casey <skythecreative@gmail.com>
2024-05-25 00:20:53 -0700

    toggle particles

--- library/particles/constellationparticles.js
+++ library/particles/constellationparticles.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('particles-canvas');
 const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
