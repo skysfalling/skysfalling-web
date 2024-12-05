8ca658f7a9774c90b6795377a08c692cd9cd32ea
~skysfall1ng~ <skythecreative@gmail.com>
2024-12-05 03:50:44 -0800

    reorg

---
+++ backend/index.js
@@ -0,0 +1,86 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const BACKEND_PORT = 8800;
+const MYSQL_HOST = "localhost";
+const MYSQL_USER = "root";
+const MYSQL_PASSWORD = "Obi1Kenobi_";
+const MYSQL_DATABASE = "skysfalling";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(BACKEND_PORT, () => {
+  console.log(`Connected to backend on port ${BACKEND_PORT}`);
+});
+
+const db = mysql.createConnection({
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend.");
+});
