2420fca203e09ecb2190f66a33d86c5ecfebd08a
~skysfall1ng~ <skythecreative@gmail.com>
2024-12-08 01:12:03 -0800

    confirm login with status confirmation

--- client/src/components/Form/Login/Login.jsx
+++ client/src/components/Form/Login/Login.jsx
@@ -1,71 +1,71 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-import { Connection } from '../../../objects/Settings';
+import { Connection } from "../../../objects/Settings";
 
-import './Login.styles.css';
+import "./Login.styles.css";
 
-const USER_DATABASE_URL = `${Connection.serverUrl}/auth/login`
+const USER_DATABASE_URL = `${Connection.serverUrl}/auth/login`;
 
 function Login() {
+  const [loginSuccess, setLoginSuccess] = React.useState(null);
   const [loginError, setLoginError] = React.useState(null);
 
   const initialValues = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   };
 
   const validationSchema = Yup.object().shape({
     email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required'),
+      .email("Invalid email address")
+      .required("Email is required"),
     password: Yup.string()
-      .min(8, 'Password must be at least 8 characters long')
-      .required('Password is required'),
+      .min(8, "Password must be at least 8 characters long")
+      .required("Password is required"),
   });
 
   const onSubmit = async (values, { setSubmitting }) => {
+    setLoginSuccess(null);
     setLoginError(null);
-    
+
     let msg = "LOGIN : ";
 
     try {
       const response = await axios.post(USER_DATABASE_URL, values);
-      
+
       if (response.data.error) {
         setLoginError(response.data.error);
       } else {
-        msg += 'SUCCESS\n\t' + response.data.message + '\n';
+        msg += "SUCCESS\n\t" + response.data.message + "\n";
         console.log(msg, response.data);
+
+        setLoginSuccess(true);
       }
-    } 
-    catch (error) {
-      msg += 'ERROR\n\t' + error + '\n';
+    } catch (error) {
+      msg += "ERROR\n\t" + error + "\n";
 
       // Handle different types of errors
       if (error.response) {
         // Server responded with an error status
         if (error.response.data.error) {
           msg += "\tData error : " + error.response.data.error;
-        }
-        else
-        {
-          msg += '\tLogin failed.';
+        } else {
+          msg += "\tLogin failed.";
         }
       } else if (error.request) {
         // Request was made but no response received
-        msg += '\tNo response from server. Please try again.';
+        msg += "\tNo response from server. Please try again.";
       } else {
         // Something else went wrong
-        msg += '\tAn error occurred. Please try again.';
+        msg += "\tAn error occurred. Please try again.";
       }
 
+      setLoginSuccess(false);
       setLoginError(true);
       console.error(msg, values);
-    } finally {
-      setSubmitting(false);
     }
   };
 
@@ -80,6 +80,7 @@ function Login() {
       >
         {({ isSubmitting }) => (
           <Form>
+            {/* (( EMAIL FIELD )) -------- >> */}
             <div className="form-group">
               <Field
                 type="email"
@@ -94,6 +95,7 @@ function Login() {
               />
             </div>
 
+            {/* (( PASSWORD FIELD )) -------- >> */}
             <div className="form-group">
               <Field
                 type="password"
@@ -108,24 +110,32 @@ function Login() {
               />
             </div>
 
-            {loginError && (
-              <div className="form-group">
-                <div className="error-message">{loginError}</div>
-              </div>
-            )}
-
             <button
               type="submit"
               className="submit-button"
               disabled={isSubmitting}
             >
-              {isSubmitting ? 'Logging in...' : 'Login'}
+              {isSubmitting ? "Logging in..." : "Login"}
             </button>
           </Form>
         )}
       </Formik>
+
+      {/* (( STATUS MESSAGE )) -------- >> */}
+      <div className="submit-result">
+        {loginError && (
+          <div className="submit-result-error">
+            Login failed. Please try again.
+          </div>
+        )}
+        {loginSuccess && (
+          <div className="submit-result-success">
+            Login successful. Welcome back!
+          </div>
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default Login;
